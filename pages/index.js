e9f8af40a9daffefc35340e92b20580f0bf8b26a
Gurjot <87029898+gsandhu-vercel@users.noreply.github.com>
2021-07-29 16:53:05 -0700

    test

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
                 <title>{siteTitle}</title>
             </Head>
             <section className={utilStyles.headingMd}>
-                <p> Hello everyone! My name is Gurjot, during undergrad I majored in
+                <p> Hello everyone! My name is Test, during undergrad I majored in
                     Computer Science and fell in love with how coding and technological innovations continued to push the boundaries of what's possible.</p>
                 <p>
                     This was built using a tutorial which you can find here:{' '}
